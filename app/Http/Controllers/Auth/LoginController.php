63bf8711e294088b5c77c0d3b5f8ed3663025927
Gonçalo Bastos <2191206@my.ipleiria.pt>
2021-06-13 14:53:33 +0100

    Override de função de login para users bloqueados

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -7,7 +7,8 @@ use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 use Illuminate\Auth\Access\AuthorizationException;
-
+use App\Models\User;
+use Exception;
 
 class LoginController extends Controller
 {
@@ -30,6 +31,43 @@ class LoginController extends Controller
      * @var string
      */
     //protected $redirectTo = RouteServiceProvider::HOME;
+
+    public function login(Request $request)
+    {
+        try{
+            $user = User::where('email', $request->email)->firstOrFail();
+            if($user->bloqueado) {
+                //return $this->sendFailedLoginResponse($request);
+                return redirect()->route('login')
+                ->with('alert-msg', 'A sua conta encontra-se bloqueada')
+                ->with('alert-type', 'danger');
+            }
+        }catch(Exception $e){
+
+        }
+        $this->validateLogin($request);
+
+        // If the class is using the ThrottlesLogins trait, we can automatically throttle
+        // the login attempts for this application. We'll key this by the username and
+        // the IP address of the client making these requests into this application.
+        if (method_exists($this, 'hasTooManyLoginAttempts') &&
+            $this->hasTooManyLoginAttempts($request)) {
+            $this->fireLockoutEvent($request);
+
+            return $this->sendLockoutResponse($request);
+        }
+
+        if ($this->attemptLogin($request)) {
+            return $this->sendLoginResponse($request);
+        }
+
+        // If the login attempt was unsuccessful we will increment the number of attempts
+        // to login and redirect the user back to the login form. Of course, when this
+        // user surpasses their maximum number of attempts they will get locked out.
+        $this->incrementLoginAttempts($request);
+
+        return $this->sendFailedLoginResponse($request);
+    }
 
     public function redirectTo(){
         if(Auth::user()->isAdmin())
